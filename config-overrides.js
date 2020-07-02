a0a8d8ce24e7dfb25da8a795334fac8093f25226
David Morais <davidmorais92@gmail.com>
2020-07-02 14:59:27 +0100

    add: theme file

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 const {
   override,
   addWebpackAlias,
@@ -7,9 +8,12 @@ const path = require("path");
 module.exports = override(
   // add an alias for "ag-grid-react" imports
   addWebpackAlias({
+    Assets: path.resolve(__dirname, "src/assets/*"),
+    Components: path.resolve(__dirname, "src/components/*"),
     Config: path.resolve(__dirname, "src/config.js"),
-    Components: path.resolve(__dirname, "src/components/"),
-    Assets: path.resolve(__dirname, "src/assets/"),
-    Store: path.resolve(__dirname, "src/store/"),
+    Hooks: path.resolve(__dirname, "src/hooks/*"),
+    Pages: path.resolve(__dirname, "src/pages/*"),
+    Store: path.resolve(__dirname, "src/store/*"),
+    Theme: path.resolve(__dirname, "src/theme.js"),
   })
 );
---
+++ src/theme.js
@@ -0,0 +1,3 @@
+export default {
+  brandColor: "#db1e3b"
+}
