6198d6a0bf6f733bbcfc025455d1c299d947faf9
Sandro Martins <rumpere@gmail.com>
2020-08-04 18:14:34 +0100

    fix for inputText style with Icon

--- src/components/TextInput/style.js
+++ src/components/TextInput/style.js
@@ -8,23 +8,24 @@ export const InputContainer = styled.div`
     position: absolute;
     align-self: flex-end;
     background-color: white;
-    padding: 12px;
+    padding: 8px;
+    margin: 5px;
   }
-  color: ${(props) =>
+  color: ${props =>
     props.error
       ? props.theme.colors.feedback.error.default
       : props.theme.colors.darkBlue};
   > p {
-    margin: ${(props) => props.theme.margin / 2}px 0;
+    margin: ${props => props.theme.margin / 2}px 0;
   }
 `;
 
 const Input = styled.input`
   border-radius: 2px;
-  padding: ${(props) => props.theme.margin}px ${(props) =>
+  padding: ${props => props.theme.margin}px ${props =>
   props.theme.margin / 2}px;
   border: 1px solid
-    ${(props) =>
+    ${props =>
       props.error
         ? props.theme.colors.feedback.error.default
         : props.theme.colors.mediumBeige};
@@ -33,19 +34,19 @@ const Input = styled.input`
   font-weight: normal;
   font-size: 16px;
   line-height: 24px;
-  color: ${(props) => props.theme.colors.darkBlue};
+  color: ${props => props.theme.colors.darkBlue};
   outline: none;
   &:focus {
     outline: none;
     border-radius: 2px;
-    border-color: ${(props) =>
+    border-color: ${props =>
       props.error
         ? props.theme.colors.feedback.error.default
         : props.theme.colors.darkBlue};
   }
   &:disabled {
-    background-color: ${(props) => props.theme.colors.lightBeige};
-    color: ${(props) => props.theme.colors.grey};
+    background-color: ${props => props.theme.colors.lightBeige};
+    color: ${props => props.theme.colors.grey};
   },
 `;
 
--- src/router.js
+++ src/router.js
@@ -1,16 +1,11 @@
-import React, { useEffect } from "react";
-import {
-  BrowserRouter,
-  Switch,
-  Route,
-  Redirect,
-  useHistory,
-} from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import { Home, Login, App } from "Pages";
 import Navbar from "Components/Navbar";
 import Footer from "Components/Footer";
 
+// eslint-disable-next-line react/prop-types
 const AppRouter = ({ component: Component, path, props }) => {
   return <Route path={path} render={() => <Component {...props} />} />;
 };
@@ -18,16 +13,10 @@ const AppRouter = ({ component: Component, path, props }) => {
 const routes = [
   { path: "/Login", component: Login },
   { path: "/App", component: App },
-  { path: "/", component: Home },
+  { path: "/", component: Home }
 ];
 
 export default function Router() {
-  const history = useHistory();
-
-  useEffect(() => {
-    console.log("history", history);
-  }, [history]);
-
   return (
     <BrowserRouter>
       <Navbar />
@@ -35,6 +24,7 @@ export default function Router() {
         {routes.map((route, index) => {
           return (
             <AppRouter
+              key={route.path}
               path={route.path}
               isPrivate={route.isPrivate}
               component={route.component}
