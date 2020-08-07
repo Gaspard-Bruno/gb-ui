84e776463d596dcb8ae266d55b2ea07818c82a38
Sandro Martins <rumpere@gmail.com>
2020-08-07 09:02:34 +0100

    fix List Styling

--- src/components/List/styles.js
+++ src/components/List/styles.js
@@ -7,19 +7,27 @@ export const ListContainer = styled.ul`
 export const ListItens = styled.li`
   counter-increment: liCounter;
   display: flex;
-  font-size: 0.8rem;
   margin-bottom: 0.5rem;
-  ${(props) => (props.hasIcon ? iconStyle : "")}
+  font-family: Space Grotesk;
+  font-style: normal;
+  font-weight: bold;
+  align-items: center;
+  ${props => (props.hasIcon ? iconStyle : "")}
 `;
 
 const iconStyle = `
     &::before {
+      width: 15px;
+      height: 15px;
+      padding: 10px;
       content: "" counter(liCounter);
       font-weight: bold;
-      font-size: 3rem;
+      font-size: 16px;
       margin-right: 1.5rem;
-      font-family: "Abril Fatface", serif;
+      font-family: "Abril Fatface",serif;
       color: white;
-      background: red;
+      background: #F0513A;
+      border-radius: 2px;
+      text-align: center;
     }
 `;
