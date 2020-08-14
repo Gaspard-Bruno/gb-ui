db3d4de3a8c914b77f7e5475be5be4462625041f
João Horta <jpamarohorta@gmail.com>
2020-08-14 13:49:11 +0100

    feat: Layout grid (WIP)

--- src/pages/Home/HeaderTab.js
+++ src/pages/Home/HeaderTab.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useTranslate } from "polyglot-react-redux-sdk";
 
-import { Page, Row, Col, Hero } from "Components/Layout";
+import { Page, Row, Col, Hero, GridCol } from "Components/Layout";
 import Card from "Components/Card";
 import { Jumbo, Body } from "Components/Text";
 import Search from "Components/Search";
@@ -11,22 +11,21 @@ const HeaderTab = () => {
   const t = useTranslate("home");
   return (
     <Hero top bg="alt">
-      <Col size={1.5}>
+      <GridCol size={7}>
         <Card isHero justify="flex-end" align="center" bg="alt">
-          <Col size={0.7}>
-            <Jumbo>{t("title")}</Jumbo>
-            <Body>{t("subTitle")}</Body>
-            <Search placeholder={t("button.placeholder")} />
-          </Col>
+          <Jumbo>{t("title")}</Jumbo>
+          <Body>{t("subTitle")}</Body>
+          <Search placeholder={t("button.placeholder")} />
         </Card>
-      </Col>
-      <Col size={1}>
+      </GridCol>
+
+      <GridCol size={1} />
+
+      <GridCol size={7}>
         <Card justify="flex-start" align="center" isHero>
-          <Col padding="20">
-            <Logo isHero size={400} color={"home"} />
-          </Col>
+          <Logo isHero size={400} color={"home"} />
         </Card>
-      </Col>
+      </GridCol>
     </Hero>
   );
 };
