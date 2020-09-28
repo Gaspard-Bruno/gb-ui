3dcc7fb5a3d320c92047432a0c3f461d794cc032
David Morais <davidmorais92@gmail.com>
2020-09-28 17:34:38 +0100

    cleanup: remove unused components/packages

--- src/components/DropDownMenu/style.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const DropDownContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: inherit;
-`;
-
-export const ListContainer = styled.div``;
