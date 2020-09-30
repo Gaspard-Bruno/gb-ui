cf2b1b368e15802080e1e07203719f1d3620d6d5
Sandro Martins <sandro.martins@gaspardbruno.com>
2020-09-30 12:05:13 +0100

    npm test
    add babel cfg
    
    add babel
    
    .

--- config-overrides.js
+++ config-overrides.js
@@ -1,13 +1,13 @@
 /* eslint-disable */
-const { override, addWebpackAlias, useEslintRc } = require("customize-cra");
-const path = require("path");
+const { override, addWebpackAlias, useEslintRc } = require('customize-cra');
+const path = require('path');
 
 module.exports = override(
   addWebpackAlias({
-    Assets: path.resolve(__dirname, "./src/assets"),
-    Components: path.resolve(__dirname, "src/components"),
-    Config: path.resolve(__dirname, "./src/config.js"),
-    Utils: path.resolve(__dirname, "./src/utils"),
-    Theme: path.resolve(__dirname, "./src/theme.js"),
+    Assets: path.resolve(__dirname, './src/assets'),
+    Components: path.resolve(__dirname, 'src/components'),
+    Config: path.resolve(__dirname, './src/config.js'),
+    Utils: path.resolve(__dirname, './src/utils'),
+    Theme: path.resolve(__dirname, './src/theme.js'),
   })
 );
