a60defe36800786e9b62e5f687fdc958bdac9247
Sandro Martins <sandro.martins@gaspardbruno.com>
2020-10-02 17:07:57 +0100

    add schemas index

--- src/components/utils/form.js
+++ src/components/utils/form.js
@@ -1,5 +1,25 @@
 import petsitting from './petsitting.form.json';
+import babysitting from './babysitting.form.json';
+import chefAtHome from './chef_at_home.form.json';
+import cleaning from './cleaning.form.json';
+import elderCompany from './elder_company.form.json';
+import gastro_experience from './gastro_experience.form.json';
+import ironing from './ironing.form.json';
+import language_classes from './language_classes.form.json';
+import musicLessons from './music_lessons.form.json';
+import repairs from './repairs.form.json';
+import gardening from './gardening.form.json';
 
 export default {
-  petsitting
+  petsitting,
+  babysitting,
+  chefAtHome,
+  cleaning,
+  elderCompany,
+  gastro_experience,
+  ironing,
+  language_classes,
+  musicLessons,
+  repairs,
+  gardening
 };
