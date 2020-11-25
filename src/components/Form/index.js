98d5bf41321d8f27e2035531ca6ee2fca1f02f69
Sandro Martins <sandro.martins@gaspardbruno.com>
2020-11-25 18:43:02 +0000

    fix label to district (become form request)

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -237,7 +237,7 @@ const Form = ({
             <React.Fragment key={'district'}>
               <Select
                 options={districtOptions}
-                label={formik?.values[field?.label] || 'Concelho'}
+                label={field?.label || 'Concelho'}
                 error={fieldProps.error}
                 defaultValue={districtOptions?.find(
                   opt => opt.value === fieldProps.value
