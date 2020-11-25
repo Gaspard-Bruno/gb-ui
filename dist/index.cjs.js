17f51ec0a0efcfa91d9b6b809fe309e99c7d7e9f
Sandro Martins <sandro.martins@gaspardbruno.com>
2020-11-25 18:20:02 +0000

    hook: compile changes

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -21781,7 +21781,8 @@ var FileUploader = function FileUploader(_ref) {
   var title = _ref.title,
       name = _ref.name,
       action = _ref.action,
-      answers = _ref.answers;
+      answers = _ref.answers,
+      error = _ref.error;
 
   var _useState = React.useState(answers || []),
       _useState2 = _slicedToArray(_useState, 2),
@@ -21854,7 +21855,7 @@ var FileUploader = function FileUploader(_ref) {
   });
   return /*#__PURE__*/React__default['default'].createElement(React__default['default'].Fragment, {
     key: 'file-uploader'
-  }, title && /*#__PURE__*/React__default['default'].createElement(Body, null, title), /*#__PURE__*/React__default['default'].createElement(FileUploaderContainer, getRootProps(), /*#__PURE__*/React__default['default'].createElement(UploaderRowWrapper, null, /*#__PURE__*/React__default['default'].createElement(Icon, {
+  }, title && /*#__PURE__*/React__default['default'].createElement(Body, null, title), error && /*#__PURE__*/React__default['default'].createElement(ErrorText, null, error), /*#__PURE__*/React__default['default'].createElement(FileUploaderContainer, getRootProps(), /*#__PURE__*/React__default['default'].createElement(UploaderRowWrapper, null, /*#__PURE__*/React__default['default'].createElement(Icon, {
     name: "upload"
   }), /*#__PURE__*/React__default['default'].createElement(Body, {
     alt: "true"
@@ -21873,7 +21874,8 @@ FileUploader.propTypes = {
   title: propTypes.string,
   name: propTypes.string,
   action: propTypes.func,
-  answers: propTypes.object
+  answers: propTypes.object,
+  error: propTypes.string
 };
 
 function _templateObject3$d() {
@@ -22071,6 +22073,42 @@ var preferredHoursOptions = [{
 }, {
   label: '18:00',
   value: '18:00'
+}, {
+  label: '18:30',
+  value: '18:30'
+}, {
+  label: '19:00',
+  value: '19:00'
+}, {
+  label: '19:30',
+  value: '19:30'
+}, {
+  label: '20:00',
+  value: '20:00'
+}, {
+  label: '20:30',
+  value: '20:30'
+}, {
+  label: '21:00',
+  value: '21:00'
+}, {
+  label: '21:30',
+  value: '21:30'
+}, {
+  label: '22:00',
+  value: '22:00'
+}, {
+  label: '22:30',
+  value: '22:30'
+}, {
+  label: '23:00',
+  value: '23:00'
+}, {
+  label: '23:30',
+  value: '23:30'
+}, {
+  label: '24:00',
+  value: '24:00'
 }];
 var weekSelectOptions = [{
   value: 0,
@@ -22136,7 +22174,7 @@ var OfferTypeWidget = function OfferTypeWidget(_ref) {
           label: 'Recorrente',
           value: 1
         }, {
-          label: 'Pacote de Horas',
+          label: 'Pacote de Horas (Válido apenas para particulares)',
           value: 2
         }],
         hourPackageOptions: [{
@@ -26291,7 +26329,8 @@ var Form$1 = function Form(_ref) {
             file: answers['file'],
             action: function action(values) {
               return formik.setFieldValue(field.key, values);
-            }
+            },
+            error: fieldProps.error
           });
 
         case 'offer-type':
--- dist/index.esm.js
+++ dist/index.esm.js
@@ -21768,7 +21768,8 @@ var FileUploader = function FileUploader(_ref) {
   var title = _ref.title,
       name = _ref.name,
       action = _ref.action,
-      answers = _ref.answers;
+      answers = _ref.answers,
+      error = _ref.error;
 
   var _useState = useState(answers || []),
       _useState2 = _slicedToArray(_useState, 2),
@@ -21841,7 +21842,7 @@ var FileUploader = function FileUploader(_ref) {
   });
   return /*#__PURE__*/React.createElement(React.Fragment, {
     key: 'file-uploader'
-  }, title && /*#__PURE__*/React.createElement(Body, null, title), /*#__PURE__*/React.createElement(FileUploaderContainer, getRootProps(), /*#__PURE__*/React.createElement(UploaderRowWrapper, null, /*#__PURE__*/React.createElement(Icon, {
+  }, title && /*#__PURE__*/React.createElement(Body, null, title), error && /*#__PURE__*/React.createElement(ErrorText, null, error), /*#__PURE__*/React.createElement(FileUploaderContainer, getRootProps(), /*#__PURE__*/React.createElement(UploaderRowWrapper, null, /*#__PURE__*/React.createElement(Icon, {
     name: "upload"
   }), /*#__PURE__*/React.createElement(Body, {
     alt: "true"
@@ -21860,7 +21861,8 @@ FileUploader.propTypes = {
   title: propTypes.string,
   name: propTypes.string,
   action: propTypes.func,
-  answers: propTypes.object
+  answers: propTypes.object,
+  error: propTypes.string
 };
 
 function _templateObject3$d() {
@@ -22058,6 +22060,42 @@ var preferredHoursOptions = [{
 }, {
   label: '18:00',
   value: '18:00'
+}, {
+  label: '18:30',
+  value: '18:30'
+}, {
+  label: '19:00',
+  value: '19:00'
+}, {
+  label: '19:30',
+  value: '19:30'
+}, {
+  label: '20:00',
+  value: '20:00'
+}, {
+  label: '20:30',
+  value: '20:30'
+}, {
+  label: '21:00',
+  value: '21:00'
+}, {
+  label: '21:30',
+  value: '21:30'
+}, {
+  label: '22:00',
+  value: '22:00'
+}, {
+  label: '22:30',
+  value: '22:30'
+}, {
+  label: '23:00',
+  value: '23:00'
+}, {
+  label: '23:30',
+  value: '23:30'
+}, {
+  label: '24:00',
+  value: '24:00'
 }];
 var weekSelectOptions = [{
   value: 0,
@@ -22123,7 +22161,7 @@ var OfferTypeWidget = function OfferTypeWidget(_ref) {
           label: 'Recorrente',
           value: 1
         }, {
-          label: 'Pacote de Horas',
+          label: 'Pacote de Horas (Válido apenas para particulares)',
           value: 2
         }],
         hourPackageOptions: [{
@@ -26278,7 +26316,8 @@ var Form$1 = function Form(_ref) {
             file: answers['file'],
             action: function action(values) {
               return formik.setFieldValue(field.key, values);
-            }
+            },
+            error: fieldProps.error
           });
 
         case 'offer-type':
