d4bfd342782c11f193bac5b0b36c3ab430d8a4c9
David Morais <davidmorais92@gmail.com>
2021-02-26 10:23:19 +0000

    fix: avatar import statement

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Icon from 'Components/Icon';
+import Icon from '../Icon';
 
 import AvatarContainer, { AvatarImage, AvatarInitials } from './style';
 
