82d3cbd3146e086d50796463a3e37a200950a75a
David Morais <davidmorais92@gmail.com>
2021-03-02 18:14:19 +0000

    fix: icon switch cursor

--- src/components/IconSwitch/style.js
+++ src/components/IconSwitch/style.js
@@ -1,35 +1,35 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const StyledSwitch = styled.div`
-    background-color: ${props => props.theme?.colors.lightestBeige};
-    border-radius: 30px;
-    display: flex;
-    width: 90px;
-    height: 40px;
+  background-color: ${props => props.theme?.colors.lightestBeige};
+  border-radius: 30px;
+  display: flex;
+  width: 90px;
+  height: 40px;
+  cursor: pointer;
 `;
 
-
 const getActive = (active, theme) => {
-    const textColor = active ? theme?.colors.darkBlue : theme?.colors.grey;
-    return `fill: ${textColor};`
-  };
+  const textColor = active ? theme?.colors.darkBlue : theme?.colors.grey;
+  return `fill: ${textColor};`;
+};
 
 export const Left = styled.div`
-    padding: 10px;
-    border-right: 1px solid ${props => props.theme?.colors.mediumBeige};
+  padding: 10px;
+  border-right: 1px solid ${props => props.theme?.colors.mediumBeige};
 
-    path {
-        ${props => getActive(props.active, props.theme)};
-    }
+  path {
+    ${props => getActive(props.active, props.theme)};
+  }
 `;
 
 export const Right = styled.div`
-    padding: 10px;
-    ${props => getActive(props.active, props.theme)};
+  padding: 10px;
+  ${props => getActive(props.active, props.theme)};
 
-    path {
-        ${props => getActive(props.active, props.theme)};
-    }
+  path {
+    ${props => getActive(props.active, props.theme)};
+  }
 `;
 
 export default StyledSwitch;
