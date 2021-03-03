921272094f28eef5e4b044887384d65862426296
David Morais <davidmorais92@gmail.com>
2021-03-03 18:31:06 +0000

    feat: settings schema

--- src/components/utils/settingsSchema.js
+++ src/components/utils/settingsSchema.js
@@ -1,38 +1,58 @@
 const stepOne = () => {
   return [
-    { key: 'name', type: 'text', required: true, label: 'Nome' },
-    { key: 'lastName', type: 'text', required: true, label: 'Apelido' }
+    { key: 'name', type: 'text', label: 'Nome' },
+    { key: 'lastName', type: 'text', label: 'Apelido' },
+    { key: 'email', type: 'text', label: 'Email', widget: 'email' },
+    { key: 'password', type: 'text', label: 'Password', widget: 'password' }
   ];
 };
 
 const stepTwo = () => {
-  return [];
-};
-
-const stepThree = () => {
   return [
     {
-      key: 'known55',
-      label: 'Como soube da 55+',
-      required: true,
-      type: 'text-area'
+      key: 'email',
+      type: 'text',
+      label: 'Endereço de Email',
+      widget: 'email '
+    },
+    { key: 'space', widget: 'space' },
+    {
+      key: 'new-email',
+      type: 'text',
+      label: 'Novo Endereço de Email',
+      widget: 'email'
     },
     {
-      key: 'mainMotivation',
-      label:
-        'Porque quer fazer parte da 55+? Qual é a sua principal motivação?',
-      required: true,
-      type: 'text-area'
+      key: 'new-email-repeat',
+      type: 'text',
+      label: 'Repetir Endereço de Email',
+      widget: 'email'
+    },
+    { key: 'space', widget: 'space', submit: true, submitLabel: 'Actualizar' },
+    { type: 'footnote', label: 'Alteração de Password' },
+    {
+      key: 'password',
+      type: 'text',
+      label: 'Password Antiga',
+      widget: 'password'
+    },
+    { key: 'space', widget: 'space' },
+    {
+      key: 'new-password',
+      type: 'text',
+      label: 'Password Nova',
+      widget: 'password'
     },
     {
-      key: 'otherInfo',
-      label: 'Outras Informações',
-      type: 'text-area'
+      key: 'new-password-repeat',
+      type: 'text',
+      label: 'Repetir Password Nova',
+      widget: 'password'
     }
   ];
 };
 
-const stepFour = () => {
+const stepThree = () => {
   return [
     {
       key: 'known55',
@@ -62,8 +82,9 @@ const formSteps = {
   type: 'form',
   questions: steps.map((step, stepIndex) => ({
     type: 'object',
-    key: `become-form-${stepIndex}`,
-    groupBy: 2,
+    submit: true,
+    submitLabel: 'Actualizar',
+    key: `settings-form-${stepIndex}`,
     label: STEP_LABELS[stepIndex],
     questions: step
   }))
