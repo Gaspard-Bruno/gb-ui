6ed5c01aa686e661835403c6e73c3db52225fa37
Sandro Martins <sandro.martins@gaspardbruno.com>
2021-04-07 15:59:11 +0100

    fix typo on badges content

--- src/content.js
+++ src/content.js
@@ -1,63 +1,62 @@
 export default {
   nav: {
-    services: "Serviços",
-    aboutUs: "Sobre Nós",
-    registerAs: "Regista-te como 55+",
-    login: "Iniciar Sessão",
-    country: "Lisboa"
+    services: 'Serviços',
+    aboutUs: 'Sobre Nós',
+    registerAs: 'Regista-te como 55+',
+    login: 'Iniciar Sessão',
+    country: 'Lisboa'
   },
   login: {
-    login: "Iniciar Sessão",
-    cancel: "Cancelar",
-    recoverPwd: "Recuperar Palavra Chave",
-    email: "Endereço de e-mail",
-    password: "Palavra Chave",
-    forgotPw: "Esqueceu a sua senha?"
+    login: 'Iniciar Sessão',
+    cancel: 'Cancelar',
+    recoverPwd: 'Recuperar Palavra Chave',
+    email: 'Endereço de e-mail',
+    password: 'Palavra Chave',
+    forgotPw: 'Esqueceu a sua senha?'
   },
   footer: {
     address: `Convento de São Pedro de Alcântara
     Travessa de São Pedro, 8
     1200-119 Lisboa`,
-    phone: "M. (+351) 930 55 65 75",
-    email: "info@55mais.pt",
-    copyright: "Copyright © 2020. 55+",
-    about: "Sobre",
-    services: "Serviços",
-    becomeSpecialist: "Torne-se num Especialista",
-    faqs: "FAQs",
-    tnc: "Termos e Condições",
-    pp: "Política de Privacidade",
-    lang: "Português"
+    phone: 'M. (+351) 930 55 65 75',
+    email: 'info@55mais.pt',
+    copyright: 'Copyright © 2020. 55+',
+    about: 'Sobre',
+    services: 'Serviços',
+    becomeSpecialist: 'Torne-se num Especialista',
+    faqs: 'FAQs',
+    tnc: 'Termos e Condições',
+    pp: 'Política de Privacidade',
+    lang: 'Português'
   },
   sidebar: {
-    requests: "Pedidos",
-    archive: "Arquivo",
-    candidates: "Novos Candidatos",
-    specialists: "Especialistas 55+",
-    clients: "Clientes",
-    members: "Membros",
-    team: "Equipas",
-    services: "Serviços"
+    requests: 'Pedidos',
+    archive: 'Arquivo',
+    candidates: 'Novos Candidatos',
+    specialists: 'Especialistas 55+',
+    clients: 'Clientes',
+    members: 'Membros',
+    team: 'Equipas',
+    services: 'Serviços'
   },
   badges: {
-    contact: "Contacto",
-    received: "Recebido",
-    complete: "Completo",
-    closed: "Fechado",
-    analysis: "Análise",
-    awaiting_specialist: "Aguarda Especialista",
-    awaiting_visit_payment: "Aguarda Pagamento Visito",
-    awaiting_interview: "Aguarda Entrevista",
-    awaiting_payment: "Aguarda Pagamento Serviço",
-    awaiting_conclusion: "Aguarda Conclusão",
-    awaiting_visit: "Aguarda Visita",
-    awaiting_conclusion: "Aguarda Conclusão",
-    canceled: "Cancelado",
-    rejected: "Rejeitado",
-    new_candidate: "Candidato",
-    awaiting_details: "Aguarda Detalhes",
-    reopened: "Reaberto",
-    accepted: "Aceite"
+    contact: 'Contacto',
+    received: 'Recebido',
+    complete: 'Completo',
+    closed: 'Fechado',
+    analysis: 'Análise',
+    awaiting_specialist: 'Aguarda Especialista',
+    awaiting_visit_payment: 'Aguarda Pagamento Visito',
+    awaiting_interview: 'Aguarda Entrevista',
+    awaiting_payment: 'Aguarda Pagamento Serviço',
+    awaiting_conclusion: 'Aguarda Conclusão',
+    awaiting_visit: 'Aguarda Visita',
+    cancelled: 'Cancelado',
+    rejected: 'Rejeitado',
+    new_candidate: 'Candidato',
+    awaiting_details: 'Aguarda Detalhes',
+    reopened: 'Reaberto',
+    accepted: 'Aceite'
   },
   requests: {
     services: 'Serviços',
@@ -74,18 +73,18 @@ export default {
     cancelled: 'Cancelado'
   },
   archive: {
-    archive: "Arquivo",
-    loading: "Loading",
-    services: "Serviços",
-    serviceType: "Tipo de Serviço",
-    client: "Cliente",
-    provider: "Especialista 55+",
-    date: "Datas",
-    totalHours: "Total de Horas",
-    totalPrice: "Montante Total",
-    hours: "Horas",
-    page: "Página",
-    of: "de"
+    archive: 'Arquivo',
+    loading: 'Loading',
+    services: 'Serviços',
+    serviceType: 'Tipo de Serviço',
+    client: 'Cliente',
+    provider: 'Especialista 55+',
+    date: 'Datas',
+    totalHours: 'Total de Horas',
+    totalPrice: 'Montante Total',
+    hours: 'Horas',
+    page: 'Página',
+    of: 'de'
   },
   candidates: {
     specialists: 'Especialistas 55+',
@@ -100,26 +99,26 @@ export default {
     cancelled: 'Cancelado'
   },
   specialists: {
-    specialists: "Especialistas 55+",
-    name: "Nome",
-    service: "Serviço",
-    totalRequests: "Total de Pedidos",
-    totalHours: "Total de Horas",
-    totalPrice: "Montante Total",
-    rating: "Avaliação",
-    state: "Estado",
-    region: "Região"
+    specialists: 'Especialistas 55+',
+    name: 'Nome',
+    service: 'Serviço',
+    totalRequests: 'Total de Pedidos',
+    totalHours: 'Total de Horas',
+    totalPrice: 'Montante Total',
+    rating: 'Avaliação',
+    state: 'Estado',
+    region: 'Região'
   },
   clients: {
-    clients: "Clientes",
-    region: "Região",
-    name: "Nome"
+    clients: 'Clientes',
+    region: 'Região',
+    name: 'Nome'
   },
   filters: {
-    responsible: "Responsável",
-    date: "Data",
-    service: "Serviço",
-    status: "Estado",
-    region: "Região"
+    responsible: 'Responsável',
+    date: 'Data',
+    service: 'Serviço',
+    status: 'Estado',
+    region: 'Região'
   }
 };
